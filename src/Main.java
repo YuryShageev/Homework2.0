1d7042e74cb53a8f47aee9b07b271e98802ec877
YuryShageev <schageev.yur@yandex.ru>
2022-09-20 15:26:03 +0300

    пробую делать с разбором домашки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Task 1;
+        System.out.println("Task 1");
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        //Task 2
+        System.out.println("Task 2");
+        dog = dog + 4;
+        System.out.println(dog);
+        cat +=4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
+        //Task 3
+        System.out.println("Task 3");
+        var dog1 = 8.0;
+        dog1 = dog1 - 3.5;
+        System.out.println(dog1);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper -= 7639;
+        System.out.println(paper);
     }
 }
